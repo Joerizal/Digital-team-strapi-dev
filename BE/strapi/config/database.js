2304e170e1d2c31ecca16930712b5aaa9d08a2db
MuhamadSyamimIrfanAhmadShokkri <syamim@seetrustudio.com>
2025-05-16 11:56:48 +0800

    test

--- BE/strapi/config/database.js
+++ BE/strapi/config/database.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 module.exports = ({ env }) => {
   const client = env('DATABASE_CLIENT', 'postgres');
-
+  // console.log("DB HOST", env('DATABASE_HOST'))
   const connections = {
     postgres: {
       connection: {
@@ -12,13 +12,16 @@ module.exports = ({ env }) => {
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
         password: env('DATABASE_PASSWORD', 'strapi'),
+        // ssl: env.bool('DATABASE_SSL', false) && {
+        //   key: env('DATABASE_SSL_KEY', undefined),
+        //   cert: env('DATABASE_SSL_CERT', undefined),
+        //   ca: env('DATABASE_SSL_CA', undefined),
+        //   capath: env('DATABASE_SSL_CAPATH', undefined),
+        //   cipher: env('DATABASE_SSL_CIPHER', undefined),
+        //   rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+        // },
         ssl: env.bool('DATABASE_SSL', false) && {
-          key: env('DATABASE_SSL_KEY', undefined),
-          cert: env('DATABASE_SSL_CERT', undefined),
-          ca: env('DATABASE_SSL_CA', undefined),
-          capath: env('DATABASE_SSL_CAPATH', undefined),
-          cipher: env('DATABASE_SSL_CIPHER', undefined),
-          rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
+          rejectUnauthorized: false,
         },
         schema: env('DATABASE_SCHEMA', 'public'),
       },
